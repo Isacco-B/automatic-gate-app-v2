9de0b320893430334e48bf6cc762035a436d7e24
Isacco-B <git@isaccobertoli.com>
2026-02-28 22:14:28 +0100

    ✨ Add profile setup guard to index screen in AppLayout

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,9 +1,13 @@
+import { useAuth } from "@/context";
 import { Stack } from "expo-router";
 
 export default function AppLayout() {
+  const { hasCompletedProfileSetup } = useAuth();
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Protected guard={hasCompletedProfileSetup}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack.Protected>
       <Stack.Screen name="profile" options={{ headerShown: false }} />
     </Stack>
   );
